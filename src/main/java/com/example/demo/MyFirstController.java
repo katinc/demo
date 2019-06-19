5a1aac4338b40a2536e6076fac5eadb56d2a2721
katinc <katinc2016@gmail.com>
2019-06-19 16:54:07 +0000

    my first commit

---
+++ src/main/java/com/example/demo/MyFirstController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyFirstController {
+    @RequestMapping("/add/{a}/{b}")
+    @ResponseBody public int add(@PathVariable("a") int a, @PathVariable("b") int b) {
+        return a + b;
+    }
+
+    @RequestMapping("/sub/{a}/{b}")
+    @ResponseBody public int sub(@PathVariable ("a") int a, @PathVariable("b") int b){
+        return a - b;
+    }
+
+    @RequestMapping("/div/{a}/{b}")
+    @ResponseBody public double divide(@PathVariable ("a") int a, @PathVariable("b") int b){
+        return a / b;
+    }
+
+    @RequestMapping("/mul/{a}/{b}")
+    @ResponseBody public int multiply(@PathVariable ("a") int a, @PathVariable("b") int b){
+        return a * b;
+    }
+}
